f19ce82b7acac27b0a54eeebc315351fdb8615bf
SunJong Park <ryan0507@yonsei.ac.kr>
2025-03-31 15:42:55 +0900

    DASGNN fixed

---
+++ run.sh
@@ -0,0 +1,8 @@
+python main_graph_classification.py --model SNNGIN --dataset MUTAG
+python main_graph_classification.py --model SNNGIN --dataset NCI1
+
+# SNNGAT
+# python main_graph_classification.py --model [SNNGCN/SNNGAT/SNNGIN] --dataset [dataset]
+
+
+
