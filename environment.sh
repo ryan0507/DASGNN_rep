9aafbac1bd63d1fe4b134a7b39400e2cdc4877b8
SunJong Park <ryan0507@yonsei.ac.kr>
2025-03-31 15:58:57 +0900

    environment added

---
+++ environment.sh
@@ -0,0 +1,13 @@
+conda install pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.8 -c pytorch -c nvidia
+pip install torch_geometric
+pip install texttable
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.0.0+cu118.html
+pip install tensorboard
+pip install spikingjelly
+pip install scipy
+pip install scikit-learn
+pip install tqdm
+pip install seaborn
+pip install pymongo
+pip install sshtunnel
+pip install numpy==1.24.3
--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 python main_graph_classification.py --model SNNGIN --dataset MUTAG
-python main_graph_classification.py --model SNNGIN --dataset NCI1
 
 # SNNGAT
 # python main_graph_classification.py --model [SNNGCN/SNNGAT/SNNGIN] --dataset [dataset]
